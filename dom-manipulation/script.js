6e1eac1da740faa87a91eb0cc0cd8f7bc2d5149a
Anthony Nwambu <aonwambu@gmail.com>
2025-07-02 22:26:35 +0100

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,19 +1,48 @@
-const quotes = [
-  "Persimist sees the hole, optimist sees the doughnut.",
-  "The best to plant an oaktree was 12 years ago the next perfect time to plant another is now",
-  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-  "The grass are not always greener at the other side.",
-  "There is no nobility in poverty."
-];
+const quotesByCategory = {
+  Love: [
+     "Love isn't something you find. Love is something that finds you.",
+    "Where there is love there is life.",
+    "To love and be loved is to feel the sun from both sides."
+  ],
+  success: [
+    "Success usually comes to those who are too busy to be looking for it.",
+    "Don’t be afraid to give up the good to go for the great.",
+    "Opportunities don't happen. You create them."
+  ],
+  Motivation: [
+     "Push yourself, because no one else is going to do it for you.",
+    "Dream it. Wish it. Do it.",
+    "Hard work beats talent when talent doesn't work hard."
+  ]
+};
 
+
+const categorySelect = document.getElementById("categorySelect");
 const quoteDisplay = document.getElementById("quoteDisplay");
 const newQuoteButton = document.getElementById("newQuote");
 
+function populateCategories() {
+  for (let category in quotesByCategory) {
+    const option = document.createElement("option");
+    option.value = category;
+    option.textContent = category.charAt(0).toUpperCase() + category.slice(1);
+    categorySelect.appendChild(option);
+  }
+}
+
+
 function showRandomQuote() {
+  const selectedCategory = categorySelect.value;
+  const quotes = quotesByCategory[selectedCategory];
+
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const selectedQuote = quotes[randomIndex];
+
   quoteDisplay.textContent = selectedQuote;
 }
 
 newQuoteButton.addEventListener("click", showRandomQuote);
 
+populateCategories();
+categorySelect.value = Object.keys(quotesByCategory)[0];
+showRandomQuote();
